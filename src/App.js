93a962341492740beda8bfc8be8f40cbb6919322
Jourdon <jourdonfloyd@gmail.com>
2022-03-17 12:52:31 -0400

    Create render control method on the App class

--- src/App.js
+++ src/App.js
@@ -13,14 +13,62 @@ class App extends React.Component {
       date: new Date()
     };
   }
+
+  getPage() {
+    // Determine which components are rendered.
+    let status = this.state.status;
+    let result;
+    switch (status) {
+      case 'logs': case 'index':
+        result = this.getIndexOrLogPage();
+        break;
+      case 'addLogEntry':
+        result = this.getAddLogEntryPage();
+        break;
+      case 'addIndexEntry':
+        result = this.getAddIndexEntryPage();
+        break;
+      default:
+        // Show log for current date by default.
+        result = this.getIndexOrLogPage('logs');
+    }
+    return result;
+  }
+
+  getIndexOrLogPage() {
+    return (
+      <div>
+        <EntryDisplay
+          status={this.state.status}
+          date={this.state.date}
+          userId={this.state.userId}/>
+      </div>
+    )
+  }
+
+  getAddLogEntryPage() {
+
+    return (
+      <div>
+        Placeholder
+      </div>
+    )
+  }
+
+  getAddIndexEntryPage() {
+    return (
+      <div>
+        Placeholder
+      </div>
+    )
+  }
   
   render() {
     return (
-      <EntryDisplay 
-        status={this.state.status}
-        date={this.state.date}
-        userId={this.state.userId}/>
-    )
+      <div className="appContainer">
+        {this.getPage()}
+      </div>
+    );
   }
 }
 
