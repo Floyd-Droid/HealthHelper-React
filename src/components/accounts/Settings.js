401f49a6222071c5a1c5c10dcfd652d9f174f041
Jourdon <jourdonfloyd@gmail.com>
2022-06-19 16:36:16 -0400

    Allow the user to link email/password credentials to existing account
    If the user created an account by using their Google credentials, they
    can now provide a separate email/password combination to access the same
    account.

--- src/components/accounts/Settings.js
+++ src/components/accounts/Settings.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { deleteUser, updateProfile, updateEmail, linkWithRedirect, getRedirectResult } from 'firebase/auth';
+import { deleteUser, updateProfile, updateEmail, linkWithRedirect, getRedirectResult, linkWithCredential, EmailAuthProvider } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
 import { auth, googleProvider, passwordReset } from '../../firebase';
@@ -11,8 +11,9 @@ export default function Settings(props) {
 	const { user, loading } = useContext(UserContext);
 	const [username, setUsername] = React.useState();
 	const [email, setEmail] = React.useState();
-	const [linkEmail, setLinkEmail] = React.useState();
-	const [linkPassword, setLinkPassword] = React.useState();
+
+	const [emailLinkEmail, setEmailLinkEmail] = React.useState();
+	const [emailLinkPassword, setEmailLinkPassword] = React.useState();
 
 	const navigate = useNavigate();
 
@@ -39,6 +40,15 @@ export default function Settings(props) {
 	const handlePasswordReset = async () => {
 		try {
 			await passwordReset(user.email);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	const handleLinkEmailAccount = async () => {
+		try {
+			const credential = EmailAuthProvider.credential(emailLinkEmail, emailLinkPassword);
+			await linkWithCredential(user, credential);
 		} catch (err) {
 			console.log(err);
 		}
@@ -78,27 +88,27 @@ export default function Settings(props) {
 			<div>
 				<h2>Account info</h2>
 				<p>Username</p>
-					<input
-							type="text"
-							onChange={e => setUsername(e.target.value)}
-							placeholder={user !== null ? user.displayName : ''}
-						/>
-					<Button 
-						variant='primary' 
-						onClick={handleUpdateUsername}>
-						Update
-					</Button>
-						<p>Email</p>
-					<input
+				<input
 						type="text"
-						onChange={e => setEmail(e.target.value)}
-						placeholder={user !== null ? user.email : ''}
+						onChange={e => setUsername(e.target.value)}
+						placeholder={user !== null ? user.displayName : ''}
 					/>
-					<Button 
-						variant='primary' 
-						onClick={handleUpdateEmail}>
-						Update
-					</Button>
+				<Button 
+					variant='primary' 
+					onClick={handleUpdateUsername}>
+					Update
+				</Button>
+				<p>Email</p>
+				<input
+					type="text"
+					onChange={e => setEmail(e.target.value)}
+					placeholder={user !== null ? user.email : ''}
+				/>
+				<Button 
+					variant='primary' 
+					onClick={handleUpdateEmail}>
+					Update
+				</Button>
 			</div>
 			<div>
 				<h2>Reset Password</h2>
@@ -112,6 +122,30 @@ export default function Settings(props) {
 						variant='primary' 
 						onClick={handleLinkGoogleAccount}>
 						Link Google Account
+					</Button>
+				</div>
+				<div>
+				<h4>Link with Email and password</h4>
+				<p>
+					If you created your account using Google or Github, and 
+					would like to use different credentials to log in, you can 
+					provide an email and password here. You will then be able 
+					to log in using either set of credentials.
+				</p>
+				<p>Email</p>
+				<input
+					type="text"
+					onChange={e => setEmailLinkEmail(e.target.value)}
+				/>
+				<p>Password</p>
+				<input
+					type="text"
+					onChange={e => setEmailLinkPassword(e.target.value)}
+				/>
+					<Button 
+						variant='primary' 
+						onClick={handleLinkEmailAccount}>
+						Add these credentials
 					</Button>
 				</div>
 				<div>
